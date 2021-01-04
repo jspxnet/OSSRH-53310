d27b1d64ba3c94fba518feeaf89045e19a1f413b
chenyuan <39793751@qq.com>
2021-01-04 23:17:03 +0800

    分布式通讯调整

--- src/main/java/jspx/example/remote/SpringPersionTcp.java
+++ src/main/java/jspx/example/remote/SpringPersionTcp.java
@@ -1,15 +1,9 @@
 package jspx.example.remote;
 
 import com.github.jspxnet.sioc.annotation.RpcClient;
-import com.github.jspxnet.txweb.annotation.Param;
 import com.github.jspxnet.txweb.enums.RpcProtocolEnumType;
-import jspx.example.conf.Persion;
 import jspx.example.controller.SpringPersionInterface;
-import jspx.example.dto.DemoDto;
 import jspx.example.env.DemoIoc;
-import jspx.example.pqo.DemoParamReq;
-
-import java.io.Serializable;
 
 /**
  * Created by jspx.net
@@ -18,7 +12,7 @@ import java.io.Serializable;
  * date: 2020/6/30 22:14
  *
  **/
-@RpcClient(bind = SpringPersionInterface.class,namespace = DemoIoc.namespace, protocol = RpcProtocolEnumType.TCP)
+@RpcClient(bind = SpringPersionInterface.class,namespace = DemoIoc.namespace, url = "/demo/persion/index",protocol = RpcProtocolEnumType.TCP)
 public interface SpringPersionTcp extends SpringPersionInterface {
 
 
